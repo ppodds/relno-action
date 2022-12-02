6b12b40adc9a233ed0580f53f9f3e92d55cd2ca4
ppodds <oscar20020629@gmail.com>
2022-12-02 23:18:47 +0800

    feat: make the project as a github action

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { Generator } from "./generator/generator";
 import { compareCommit } from "./git/log";
 import { Config } from "./config/config";
-import { getInput, setOutput } from "@actions/core";
+import { getInput, info, setFailed, setOutput } from "@actions/core";
 
 async function run() {
-  const config = new Config();
-  await config.load();
-  const from = getInput("from", { required: true });
-  const to = getInput("to", { required: true });
-  const log = await compareCommit(from, to);
-  const generator = new Generator(log, {
-    template: config.template ?? "",
-    prTypes: config.prTypes ?? [],
-  });
-  setOutput("out", generator.generate());
+  try {
+    const config = new Config();
+    await config.load();
+    const from = getInput("from", { required: true });
+    const to = getInput("to", { required: true });
+    const log = await compareCommit(from, to);
+    const generator = new Generator(log, {
+      template: config.template ?? "",
+      prTypes: config.prTypes ?? [],
+    });
+    const result = generator.generate();
+    info(`Generated release note:\n${result}`);
+    setOutput("out", result);
+  } catch (e) {
+    setFailed(`Action failed with error: ${e}`);
+  }
 }
 
 run();
